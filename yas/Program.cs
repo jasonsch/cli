744f990a3f605929449d92d702aa219a82b332dc
Jason Schroeder <jasonsch@gmail.com>
2018-11-26 17:12:14 -0800

    Add 'yas' program

---
+++ yas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace yas
+{
+    class Program
+    {
+        const string DefaultOutputString = "queen";
+
+        static void Main(string[] args)
+        {
+            string ToPrint;
+
+            if (args.Length > 0)
+            {
+                ToPrint = args[0];
+            }
+            else
+            {
+                ToPrint = DefaultOutputString;
+            }
+
+            PrintString(ToPrint);
+        }
+
+        static void PrintString(string OutputString)
+        {
+            while (true)
+            {
+                Console.WriteLine($"{OutputString}");
+            }
+        }
+    }
+}
