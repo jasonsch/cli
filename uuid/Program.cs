8948d4631e7fd66e372e9f2c988269e3676bde20
Jason Schroeder <jasonsch@gmail.com>
2019-06-28 13:59:59 -0700

    Add ability to validate a GUID

--- uuid/Program.cs
+++ uuid/Program.cs
@@ -6,7 +6,16 @@ namespace uuid
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(System.Guid.NewGuid().ToString());
+            if (args.Length == 0)
+            {
+                Console.WriteLine(System.Guid.NewGuid().ToString());
+            }
+            else
+            {
+                Guid uuid;
+                bool IsValidGUID = Guid.TryParse(args[0], out uuid);
+                Console.WriteLine($"'{args[0]}' is " + (IsValidGUID ? "" : "NOT ") + "a valid GUID");
+            }
         }
     }
 }
