d2eb66663db18b88e1bdfe1a84ddccffdabe4993
Jason Schroeder <jasonsch@gmail.com>
2019-08-14 21:43:51 -0700

    Add mv and flatten projects

---
+++ flatten/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace flatten
+{
+    class Program
+    {
+        static void PrintUsage()
+        {
+            Console.WriteLine("Invalid usage!");
+            Console.WriteLine("Usage: flatten <dir>");
+            Environment.Exit(0);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1 || !Directory.Exists(args[0]))
+            {
+                PrintUsage();
+            }
+
+            FlattenDirectory(args[0]);
+        }
+
+        static void FlattenDirectory(string directory)
+        {
+            foreach (var subDirectory in Directory.EnumerateDirectories(directory))
+            {
+                FlattenDirectoryWorker(directory, subDirectory);
+            }
+        }
+
+        static void FlattenDirectoryWorker(string destination, string source)
+        {
+            foreach (var subDirectory in Directory.EnumerateDirectories(source))
+            {
+                FlattenDirectoryWorker(destination, subDirectory);
+            }
+
+            foreach (var file in Directory.EnumerateFiles(source))
+            {
+                MoveFile(destination, file);
+            }
+
+            Directory.Delete(source);
+        }
+
+        private static bool AreFilesEqual(string file1, string file2)
+        {
+            FileInfo fileInfo1 = new FileInfo(file1);
+            FileInfo fileInfo2 = new FileInfo(file2);
+
+            if (fileInfo1.Length == fileInfo2.Length)
+            {
+                return File.ReadAllBytes(file1).SequenceEqual(File.ReadAllBytes(file2));
+            }
+
+            return false;
+        }
+
+        private static void MoveFile(string destination, string file)
+        {
+            string fileName = Path.GetFileName(file);
+
+            if (File.Exists(Path.Combine(destination, fileName)))
+            {
+                if (AreFilesEqual(Path.Combine(destination, fileName), file))
+                {
+                    File.Delete(file);
+                    return;
+                }
+
+                // TODO -- Only decorate the file with as much path needed to make it distinct.
+                fileName = RelativePath(destination, file);
+            }
+
+            File.Move(file, Path.Combine(destination, fileName));
+        }
+
+        //
+        // TODO
+        // Source will be something like "c:\a\b\c\d\e" and destination will be something like "c:\a\b" (i.e., destination is always an ancestor of source).
+        //
+        // TODO -- There's got to be a better way to do this.
+        //
+        static string RelativePath(string ancestorPath, string childPath)
+        {
+            string[] sourceComponents = childPath.Split(Path.DirectorySeparatorChar);
+            string[] destComponents = ancestorPath.Split(Path.DirectorySeparatorChar);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = destComponents.Length; i < sourceComponents.Length; ++i)
+            {
+                sb.Append(sourceComponents[i]);
+                sb.Append('-');
+            }
+
+            sb.Remove(sb.Length - 1, 1);
+            return sb.ToString();
+        }
+    }
+}
---
+++ mv/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace mv
+{
+    class Program
+    {
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Invalid parameters ...");
+            Console.WriteLine("Usage: ");
+            Console.WriteLine("mv <file1> <file2>");
+            Console.WriteLine("mv <files> <directory>");
+
+            Environment.Exit(0);
+        }
+
+        static void Main(string[] args)
+        {
+            string Destination;
+
+            if (args.Length < 2)
+            {
+                PrintUsage();
+                return;
+            }
+
+            //
+            // There are two scenarios: Either we're renaming one file to another or we're
+            // moving one or more files into a directory (can't be a file).
+            //
+            Destination = args[args.Length - 1];
+            if (Directory.Exists(Destination))
+            {
+                for (int i = 0; i < args.Length - 1; ++i)
+                {
+                    //
+                    // Check if this is an actual file. Otherwise, we'll assume it's a wildcard expression.
+                    //
+                    if (File.Exists(args[i]))
+                    {
+                        MoveFile(args[i], Path.Combine(Destination, Path.GetFileName(args[i])));
+                    }
+                    else
+                    {
+                        foreach (string file in Directory.EnumerateFiles(".", args[i]))
+                        {
+                            MoveFile(file, Path.Combine(Destination, Path.GetFileName(file)));
+                        }
+                    }
+                }
+            }
+            else
+            {
+                //
+                // If we have more than two parameters than the last one needed to be a directory.
+                //
+                if (args.Length > 2)
+                {
+                    PrintUsage();
+                }
+
+                MoveFile(args[0], args[1]);
+            }
+        }
+
+        static void MoveFile(string SourceFile, string DestFile)
+        {
+            if (File.Exists(DestFile))
+            {
+                File.Delete(DestFile);
+            }
+
+            File.Move(SourceFile, DestFile);
+        }
+    }
+}
