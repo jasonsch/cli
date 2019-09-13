49fcf2375713d0ac368421a6bda9b18be9f1b6be
Jason Schroeder <jasonsch@gmail.com>
2019-09-12 22:34:42 -0700

    Add sleep command

---
+++ sleep/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace sleep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                PrintUsage();
+            }
+
+            System.Threading.Thread.Sleep(Convert.ToInt32(args[0]) * 1000);
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Invalid usage!");
+            Console.WriteLine("sleep <seconds to sleep>");
+            Environment.Exit(0);
+        }
+    }
+}
